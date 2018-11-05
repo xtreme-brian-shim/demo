efe6598d870194b24db17f5fa8f5f0b0c9c2ddf0
Brian S <the.brian.shim@gmail.com>
2018-11-05 15:27:39 -0500

    Revert "commit failing test"
    This reverts commit 28f79bec5ea595b42ef369ddcc52fc5361be41f2.

--- src/test/java/com/eq/demo/controller/UserInfoControllerTest.java
+++ src/test/java/com/eq/demo/controller/UserInfoControllerTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -35,8 +34,7 @@ public class UserInfoControllerTest {
 
     @Test
     public void testRoot() throws Exception {
-//        mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(content().string("Home Page"));
-        assertTrue(false);
+        mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(content().string("Home Page"));
     }
 }
 
