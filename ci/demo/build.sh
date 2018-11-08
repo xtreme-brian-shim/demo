19a0f45cbbd3fb36f045cafb5b453f44e1d47fd7
Brian S <the.brian.shim@gmail.com>
2018-11-08 14:45:20 -0500

    trigger deployment

--- ci/demo/build.sh
+++ ci/demo/build.sh
@@ -6,8 +6,8 @@ export PROJECT_ROOT=$(pwd)
 export GRADLE_USER_HOME="$PROJECT_ROOT/.gradle"
 pushd $1
 ./gradlew build -x test
-#popd
-mv "$1/build/libs/*-0.0.1-SNAPSHOT.jar" deploy/
+popd
+mv "$1/build/libs/demo-0.0.1-SNAPSHOT.jar" deploy/
 mv "$1/ci/demo/manifest.yml" deploy/
-local_route = `echo ${route} | sed 's|https://||g'`
+local_route=`echo ${route} | sed 's|https://||g'`
 sed -i "s/\[ROUTE\]/${local_route}/g" deploy/manifest.yml
